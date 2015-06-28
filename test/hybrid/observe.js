3f2263e4d2c76e901c7dac57f74dcdd653b33a93
Andreas Karlsson <sprawl@onlajn.nu>
2015-06-28 17:53:15 +0200

    added observer support to hybrid

---
+++ test/hybrid/observe.js
@@ -0,0 +1,86 @@
+var should = require('should');
+
+var ViewDb = require('viewdb');
+var HybridStore = require('../..').Hybrid;
+
+
+describe('Observe', function() {
+	var local = null;
+	var remote = null;
+	var hybrid = null;
+
+	beforeEach(function(done){
+		local = new ViewDb();
+		remote = new ViewDb();
+		hybrid = new ViewDb(new HybridStore(local, remote, {throttleObserveRefresh:20}));
+		hybrid.open().then(function() {done()});
+	});
+
+	it('#observe with local insert', function(done) {
+		var cursor = hybrid.collection('dollhouse').find({});
+		var handle = cursor.observe({
+			added:function(x) {
+				x._id.should.equal('echo');
+				handle.stop();
+				done();
+			}
+		});
+		local.collection('dollhouse').insert({_id:'echo'});
+	});
+	it('#observe with query and local insert', function(done) {
+		remote.collection('dollhouse').insert({_id:'echo'});
+		var cursor = hybrid.collection('dollhouse').find({_id:'echo2'});
+		var handle = cursor.observe({
+			added:function(x) {
+				x._id.should.equal('echo2');
+				handle.stop();
+				done();
+			}
+		});
+		local.collection('dollhouse').insert({_id:'echo2'});
+	});	
+	it('#observe called twice with one local and one remote insert', function(done) {
+		remote.collection('dollhouse').insert({_id:'echo'});
+		var cursor = hybrid.collection('dollhouse').find({_id:'echo2'});
+		var called = 0;
+		var handle = cursor.observe({
+			added:function(x) {
+				console.log("added");
+				if(++called===2) {
+					done();
+				}
+			},
+			changed:function(x) {
+				console.log("changed");
+				if(++called===2) {
+				handle.stop();
+					done();
+				}
+			}
+		});
+		local.collection('dollhouse').insert({_id:'echo2'});
+		remote.collection('dollhouse').insert({_id:'echo2', remote:true});
+
+	});	
+	xit('#observe with query and update', function(done) {
+		var store = new ViewDb();
+		store.open().then(function() {
+				var cursor = store.collection('dollhouse').find({_id:'echo'});
+				var handle = cursor.observe({
+					added:function(x) {
+						x.age.should.equal(10);
+						x._id.should.equal('echo');
+					}, changed:function(o,n) {
+						o.age.should.equal(10);
+						n.age.should.equal(100);
+						handle.stop();
+						done();
+					}
+				});
+			
+			store.collection('dollhouse').insert({_id:'echo', age:10}, function(){
+				store.collection('dollhouse').save({_id:'echo', age:100});
+			});			
+		});
+	});	
+})
