efec5dd346987a38f42ab50a63dc1d9d5fdb678b
Thomas <tbruun@gmail.com>
2016-08-23 15:54:39 +0200

    removing hitcounter

--- test/rest_client/client.js
+++ test/rest_client/client.js
@@ -3,6 +3,7 @@ var Client = require('../..').RestClient;
 var Store = require('../..').Client;
 var should = require('should');
 var mockResponse = require('./mock-response.json');
+var _ = require('lodash');
 
 var testOptions = {
   pollInterval: 15
@@ -35,79 +36,73 @@ describe('RestClient', function () {
 
   it('#observe should work', function (done) {
     var restClient = new Client('http://www.example.com/', {}, testOptions);
-    var hitCount = 0;
-
+    var handle;
+    var realDone = _.after(2, function () {
+      handle.stop();
+      done();
+    });
     nock('http://www.example.com')
       .persist() // keep nock alive after first call
       .get('/shipment?q=%7B%22name%22%3A%22a%22%7D&skip=1&limit=100')
       .reply(function () {
-        hitCount++;
+        realDone();
         return [201, mockResponse, {}];
       });
 
     // {observe:this._query, collection:this._collection._name, events:events}
-    restClient.subscribe({ observe: { name: 'a' }, collection: 'shipment', events: {}, skip: 1, limit: 100 }, function () {});
-
-    // wait 70ms to see if >=5 calls were made (initial request at 0ms)
-    setTimeout(function () {
-      should.equal(true, (hitCount >= 5));
-      done();
-    }, 70);
+    handle = restClient.subscribe({ observe: { name: 'a' }, collection: 'shipment', events: {}, skip: 1, limit: 100 }, function () {});
   });
 
   it('#observe should stop when calling stop', function (done) {
     var restClient = new Client('http://www.example.com/', {}, testOptions);
     var hitCount = 0;
+    var stop;
 
     nock('http://www.example.com')
       .persist() // keep nock alive after first call
       .get('/parcel?q=%7B%22name%22%3A%22a%22%7D')
       .reply(function () {
         hitCount++;
+        stop();
         return [201, mockResponse, {}];
       });
 
     var observer = restClient.subscribe({ observe: { name: 'a' }, collection: 'parcel', events: {} }, function () {});
-
-    // stop observerver after 15ms, should be just enough for 2 calls (initial call at 0ms)
-    setTimeout(function () {
-      observer.stop();
-    }, 15);
+    stop = _.after(1, observer.stop)
 
     setTimeout(function () {
-      hitCount.should.equal(2);
+      hitCount.should.equal(1);
       done();
-    }, 20);
+    }, 15);
   });
 
   it('#observe should notify changes', function (done) {
     var restClient = new Client('http://www.example.com', {}, testOptions);
-    var hitCount = 0;
 
     // mock returning response with data - dies after one hit
     nock('http://www.example.com')
       .get('/party?q=%7B%22name%22%3A%22a%22%7D')
-      .reply(function () {
-        hitCount++;
-        return [201, mockResponse, {}];
-      });
+      .reply(201, mockResponse);
     // mock returning empty response
     nock('http://www.example.com')
       .get('/party?q=%7B%22name%22%3A%22a%22%7D')
-      .reply(function () {
-        hitCount++;
-        return [201, {}, {}];
-      });
+      .reply(201, {});
 
-    restClient.subscribe({ observe: { name: 'a' }, collection: 'party', events: {} }, function (err, res) {
-      if (hitCount === 1) {
+    var hits = 0;
+    var verify = function (res) {
+      ++hits;
+      if (hits === 1) {
         should.equal(res.changes[0].a.e.name, 'firstName');
         should.equal(res.changes.length, 1);
       }
-      if (hitCount === 2) {
+      if (hits === 2) {
         should.equal(res.changes[0].r.e.name, 'firstName');
         done();
       }
+    };
+
+    restClient.subscribe({ observe: { name: 'a' }, collection: 'party', events: {} }, function (err, res) {
+      verify(res);
     });
   });
 });
